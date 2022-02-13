d1f5cbdbc6348577a08444a627945f1d0cbf9c21
evansloan <evansloan082@gmail.com>
2022-02-12 22:46:20 -0500

    update item migration

--- migrations/20220213023106-create-collection-log-item.js
+++ migrations/20220213023106-create-collection-log-item.js
@@ -57,6 +57,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('collection_log_entry');
+    await queryInterface.dropTable('collection_log_item');
   }
 };
