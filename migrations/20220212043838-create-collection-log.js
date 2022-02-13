933281e41361e5a740f6b2561435dc3af228508d
evansloan <evansloan082@gmail.com>
2022-02-12 22:17:08 -0500

    update collection log migration

--- migrations/20220212043838-create-collection-log.js
+++ migrations/20220212043838-create-collection-log.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.DataTypes.UUID,
         references: {
           model: {
-            tableName: 'users',
+            tableName: 'user',
           },
           key: 'id'
         },
