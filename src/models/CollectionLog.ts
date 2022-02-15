c18d05e92b076b73f1346153a8d5518644cfa814
evansloan <evansloan082@gmail.com>
2022-02-14 22:50:07 -0500

    add method to serialize collection log to json

--- src/models/CollectionLog.ts
+++ src/models/CollectionLog.ts
@@ -10,8 +10,10 @@ import {
   Table
 } from 'sequelize-typescript';
 
+import CollectionLogEntry from './CollectionLogEntry';
 import CollectionLogItem from './CollectionLogItem';
 import CollectionLogKillCount from './CollectionLogKillCount';
+import CollectionLogTab from './CollectionLogTab';
 import CollectionLogUser from './CollectionLogUser';
 
 @Table({
@@ -38,6 +40,58 @@ class CollectionLog extends Model {
 
   @HasMany(() => CollectionLogKillCount)
   killCounts?: CollectionLogKillCount[];
+
+  jsonData = async () => {
+    // Grab the items from entry records to make looping
+    // through the data a bit easier
+    const collectionLogEntries = await CollectionLogEntry.findAll({
+      include: [
+        CollectionLogTab, {
+        model: CollectionLogItem,
+        where: {
+          collectionLogId: this.id,
+        }
+      }, {
+        model: CollectionLogKillCount,
+        where: {
+          collectionLogId: this.id,
+        },
+      }],
+    });
+  
+    let data: any = {
+      collectionlog_id: this.id,
+      user_id: this.userId,
+      tabs: {},
+    };
+  
+    collectionLogEntries.forEach((entry) => {
+      const tabName = entry.tab.name;
+  
+      if (!data.tabs.hasOwnProperty(tabName)) {
+        data.tabs[tabName] = {};
+      }
+  
+      data.tabs[tabName][entry.name] = {
+        items: entry.items?.map((item) => {
+          return {
+            id: item.itemId,
+            name: item.name,
+            quantity: item.quantity,
+            obtained: item.obtained,
+          }
+        }),
+        kill_count: entry.killCounts?.map((killCount) => {
+          return {
+            name: killCount.name,
+            amount: killCount.amount,
+          };
+        }),
+      }
+    });
+
+    return data;
+  }
 }
 
 export default CollectionLog;
