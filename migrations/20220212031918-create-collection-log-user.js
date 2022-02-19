47e939ebb89119793b0d63b31d4e078af54b9c37
evansloan <evansloan082@gmail.com>
2022-02-19 15:31:43 -0500

    drop proper table in user migration

--- migrations/20220212031918-create-collection-log-user.js
+++ migrations/20220212031918-create-collection-log-user.js
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('user');
+    await queryInterface.dropTable('collection_log_user');
   }
 };
