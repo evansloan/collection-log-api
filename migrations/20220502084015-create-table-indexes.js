dd6f29cc0bddc74298a27af36969fc55c71337e6
Evan Sloan <evan@evans.loan>
2022-05-04 22:52:08 +0000

    update index migration

--- migrations/20220502084015-create-table-indexes.js
+++ migrations/20220502084015-create-table-indexes.js
@@ -2,16 +2,14 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    queryInterface.addIndex('collection_log', ['id']);
-    queryInterface.addIndex('collection_log_user', ['id']);
-    queryInterface.addIndex('collection_log_item', ['id', 'collection_log_id']);
-    queryInterface.addIndex('collection_log_kill_count', ['id', 'collection_log_id']);
+    queryInterface.addIndex('collection_log_item', ['collection_log_id']);
+    queryInterface.addIndex('collection_log_item', ['collection_log_entry_id', 'collection_log_id']);
+    queryInterface.addIndex('collection_log_kill_count', ['collection_log_entry_id', 'collection_log_id']);
   },
 
   async down (queryInterface, Sequelize) {
-    queryInterface.removeIndex('collection_log', ['id']);
-    queryInterface.removeIndex('collection_log_user', ['id']);
-    queryInterface.removeIndex('collection_log_item', ['id', 'collection_log_id']);
-    queryInterface.removeIndex('collection_log_kill_count', ['id', 'collection_log_id']);
+    queryInterface.removeIndex('collection_log_item', ['collection_log_id']);
+    queryInterface.removeIndex('collection_log_item', ['collection_log_entry_id', 'collection_log_id']);
+    queryInterface.removeIndex('collection_log_kill_count', ['collection_log_entry_id', 'collection_log_id']);
   }
 };
