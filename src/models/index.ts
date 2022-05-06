4cf192e3911524e268fa73a411de1f1a26ddcb75
evansloan <evan@evans.loan>
2022-05-05 22:14:24 -0400

    add index file for models

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+import CollectionLog from '@models/CollectionLog';
+import CollectionLogEntry from '@models/CollectionLogEntry';
+import CollectionLogItem from '@models/CollectionLogItem';
+import CollectionLogKillCount from '@models/CollectionLogKillCount';
+import CollectionLogTab from '@models/CollectionLogTab';
+import CollectionLogUser from '@models/CollectionLogUser';
+
+export {
+  CollectionLog,
+  CollectionLogEntry,
+  CollectionLogItem,
+  CollectionLogKillCount,
+  CollectionLogTab,
+  CollectionLogUser,
+};
