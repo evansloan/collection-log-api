5c8f378847229420b0b583b3d82635876c58b212
evansloan <evan@evans.loan>
2022-06-24 23:07:02 -0400

    clean up hiscores function add user rank function add accounttype type

--- src/handlers/hiscores.ts
+++ src/handlers/hiscores.ts
@@ -1,5 +1,5 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { Op } from 'sequelize';
+import { APIGatewayProxyEvent, APIGatewayProxyHandlerV2, APIGatewayProxyResult } from 'aws-lambda';
+import { Op, QueryTypes } from 'sequelize';
 import { Sequelize } from 'sequelize-typescript';
 
 import {
@@ -11,22 +11,19 @@ import {
   CollectionLogUser,
 } from '@models/index';
 import db from '@services/database';
-import { Col, Fn } from 'sequelize/types/utils';
+import { AccountType } from '@datatypes/CollectionLogUserData';
 
 type HiscoresType = 'total' | 'unique';
+
+interface RankData {
+  username: string;
+  pos: number;
+}
 
 const headers = {
   'content-type': 'application/json',
   'Access-Control-Allow-Origin': '*',
 };
-
-const validAccountTypes = [
-  'NORMAL',
-  'IRONMAN',
-  'GROUP_IRONMAN',
-  'HARDCORE_IRONMAN',
-  'ULTIMATE_IRONMAN',
-];
 
 db.addModels([
   CollectionLog,
@@ -47,74 +44,116 @@ export const get = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyR
   }
 
   let accountTypeFilter = undefined;
-  if (accountType && validAccountTypes.indexOf(accountType) != -1) {
+  if (accountType && accountType in AccountType) {
     accountTypeFilter = {
       accountType: accountType,
     };
+
+    if (accountType == AccountType.IRONMAN) {
+      accountTypeFilter = {
+        [Op.or]: [
+          { accountType: AccountType.IRONMAN },
+          { accountType: AccountType.HARDCORE_IRONMAN },
+          { accountType: AccountType.ULTIMATE_IRONMAN },
+        ],
+      };
+    }
+
+    if (accountType == AccountType.GROUP_IRONMAN) {
+      accountTypeFilter = {
+        [Op.or]: [
+          { accountType: AccountType.GROUP_IRONMAN },
+          { accountType: AccountType.HARDCORE_GROUP_IRONMAN },
+        ],
+      };
+    }
   }
 
   const limit = 25;
   const offset = limit * (page - 1);
 
-  const obtainedDate = new Date();
-  obtainedDate.setDate(obtainedDate.getDate() - 7);
+  const obtainedCol = Sequelize.col(`"collectionLog"."${hiscoresType}_obtained"`);
+  const totalCol = Sequelize.col(`"collectionLog"."${hiscoresType}_items"`);
 
-  let recentObtained: Col | Fn = Sequelize.col('collectionLog->items.item_id');
-  if (hiscoresType == 'unique') {
-    recentObtained = Sequelize.fn('DISTINCT', recentObtained);
-  }
+  const rank = `RANK() OVER (ORDER BY ${obtainedCol.col} DESC, "collectionLog"."updated_at" ASC)`;
 
-  const hiscores = await CollectionLogUser.findAll({
-    attributes: {
-      include: [
-        [Sequelize.fn('MAX', Sequelize.col(`collectionLog.${hiscoresType}_obtained`)), 'obtained'],
-        [Sequelize.fn('MAX', Sequelize.col(`collectionLog.${hiscoresType}_items`)), 'total'],
-        [Sequelize.fn('COUNT', recentObtained), 'recent_obtained'],
-      ],
-      exclude: [
-        'id',
-        'runeliteId',
-        'accountHash',
-        'isFemale',
-        'isBanned',
-        'createdAt',
-        'updatedAt',
-        'deletedAt',
-      ],
-    },
+  const hiscores = await CollectionLogUser.scope('info').findAll({
+    attributes: [
+      [Sequelize.literal(rank), 'rank'],
+      [obtainedCol, 'obtained'],
+      [totalCol, 'total'],
+    ],
     include: [{
       model: CollectionLog,
       attributes: [],
       required: true,
-      include: [{
-        model: CollectionLogItem,
-        attributes: [],
-        where: {
-          obtained: true,
-          obtainedAt: {
-            [Op.gte]: obtainedDate,
-          },
-        },
-        duplicating: false,
-        required: false,
-      }],
     }],
     where: {
       ...accountTypeFilter,
-      runeliteId: {
-        [Op.ne]: null,
-      },
       isBanned: false,
     },
-    order: [[Sequelize.fn('MAX', `collectionLog.${hiscoresType}_obtained`), 'DESC']],
+    order: [[obtainedCol, 'DESC']],
     limit: 25,
     offset: offset,
-    group: ['CollectionLogUser.id'],
   });
 
   return {
     statusCode: 201,
     headers,
     body: JSON.stringify(hiscores),
+  };
+};
+
+export const getRankByUsername: APIGatewayProxyHandlerV2 = async (event) => {
+  const username = event.pathParameters?.username as string;
+
+  // Not a fan of using raw SQL here but using the query builder seems like it would be a pain
+  const getRank = async (type: string, username: string) => {
+    const subQuery = `
+      SELECT
+        RANK() OVER (ORDER BY "cl"."${type}_obtained" DESC, "cl"."updated_at" ASC) AS "pos",
+        "clu"."username" AS "username"
+      FROM
+        "collection_log_user" "clu"
+        JOIN "collection_log" "cl" ON "cl"."user_id" = "clu"."id"
+      WHERE
+        "clu"."is_banned" = 'f' AND
+        "clu"."deleted_at" IS NULL AND
+        "cl"."deleted_at" IS NULL
+    `;
+
+    const query = `
+      SELECT
+        "psq"."pos",
+        "psq"."username"
+      FROM (${subQuery}) "psq"
+      WHERE
+        LOWER("psq"."username") = ?
+      LIMIT 1
+    `;
+
+    const results = await db.query(query, {
+      replacements: [username.toLowerCase()],
+      type: QueryTypes.SELECT,
+    });
+
+    return results[0] as RankData;
+  };
+
+  const totalRank = await getRank('total', username);
+  const uniqueRank = await getRank('unique', username);
+
+  const resData = {
+    username: username,
+    ranks: {
+      total: totalRank.pos,
+      unique: uniqueRank.pos,
+    },
+  };
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(resData),
   };
 };
