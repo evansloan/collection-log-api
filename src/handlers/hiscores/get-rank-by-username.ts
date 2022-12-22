4c29a55462a158c45dc45e4945f385f73a94a897
evansloan <evan@evans.loan>
2022-12-22 18:55:53 -0500

    use account type filter on hiscores rank

--- src/handlers/hiscores/get-rank-by-username.ts
+++ src/handlers/hiscores/get-rank-by-username.ts
@@ -4,12 +4,14 @@ import { middleware } from '@middleware/common';
 import { DatabaseContext } from '@middleware/database';
 import { CollectionLog, CollectionLogUser } from '@models/index';
 import { response } from '@utils/handler-utils';
+import { AccountType } from '@datatypes/CollectionLogUserData';
 
 const getRankByUsername: APIGatewayProxyHandlerV2 = async (event, context) => {
   const { database: db } = context as DatabaseContext;
   const paramsUsername = event.pathParameters?.username as string;
+  const accountType = event.queryStringParameters?.accountType?.toUpperCase();
 
-  const userRanksQuery = db.select({ username: 'username' })
+  let userRanksQuery = db.select({ username: 'username' })
     .rank('rank', (qb) => {
       qb.orderBy('unique_obtained', 'DESC')
         .orderBy('collection_log.updated_at', 'ASC');
@@ -20,6 +22,23 @@ const getRankByUsername: APIGatewayProxyHandlerV2 = async (event, context) => {
     .andWhere('collection_log.deleted_at', null)
     .andWhere('collection_log_user.deleted_at', null);
 
+  if (accountType && accountType in AccountType) {
+    if (accountType == AccountType.IRONMAN) {
+      userRanksQuery = userRanksQuery.andWhere((qb) => {
+        qb.where({ account_type: AccountType.IRONMAN })
+          .orWhere({ account_type: AccountType.HARDCORE_IRONMAN })
+          .orWhere({ account_type: AccountType.ULTIMATE_IRONMAN });
+      });
+    } else if (accountType == AccountType.GROUP_IRONMAN) {
+      userRanksQuery = userRanksQuery.andWhere((qb) => {
+        qb.where({ account_type: AccountType.GROUP_IRONMAN })
+          .orWhere({ account_type: AccountType.HARDCORE_GROUP_IRONMAN });
+      });
+    } else {
+      userRanksQuery.andWhere({ account_type: accountType });
+    }
+  }
+
   const rank = await db.with('user_ranks', userRanksQuery)
     .select({
       pos: 'user_ranks.rank',
@@ -28,6 +47,10 @@ const getRankByUsername: APIGatewayProxyHandlerV2 = async (event, context) => {
     .from('user_ranks')
     .whereRaw('LOWER(username) = ?', [paramsUsername.toLowerCase()])
     .first();
+
+  if (!rank && accountType) {
+    return response(200, { rank: 0 });
+  }
 
   return response(200, { rank: rank?.pos });
 };
