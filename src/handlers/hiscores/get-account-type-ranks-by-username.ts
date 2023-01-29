58907a71fc4a63d78afed23cc79f7b48d3a62d59
Evan Sloan <evan@evans.loan>
2023-01-28 19:15:49 -0500

    update account type ranks endpoint include more account types

--- src/handlers/hiscores/get-account-type-ranks-by-username.ts
+++ src/handlers/hiscores/get-account-type-ranks-by-username.ts
@@ -29,30 +29,44 @@ const getAccountTypeRanksByUsername: APIGatewayProxyHandlerV2 = async (event, co
 
   const ironmanRanksQuery = accountTypeRanksQuery.clone()
     .whereIn('account_type', IRONMAN_TYPES);
+  const hcIronmanRanksQuery = accountTypeRanksQuery.clone()
+    .where({ account_type: AccountType.HARDCORE_IRONMAN });
+  const ultIronmanRanksQuery = accountTypeRanksQuery.clone()
+    .where({ account_type: AccountType.ULTIMATE_IRONMAN });
   const gimRanksQuery = accountTypeRanksQuery.clone()
     .whereIn('account_type', GROUP_IRONMAN_TYPES);
+  const hcGimRanksQuery = accountTypeRanksQuery.clone()
+    .where({ account_type: AccountType.HARDCORE_GROUP_IRONMAN });
   const normalRanksQuery = accountTypeRanksQuery.clone()
     .where({ account_type: AccountType.NORMAL });
 
   const ranks = await db.with('all_ranks', allRanksQuery)
     .with('ironman_ranks', ironmanRanksQuery)
+    .with('hc_ironman_ranks', hcIronmanRanksQuery)
+    .with('ult_ironman_ranks', ultIronmanRanksQuery)
     .with('gim_ranks', gimRanksQuery)
+    .with('hc_gim_ranks', hcGimRanksQuery)
     .with('normal_ranks', normalRanksQuery)
     .select({
-      username: 'all_ranks.username',
       rank: 'all_ranks.rank',
       ironmanRank: 'ironman_ranks.rank',
+      hardcoreIronmanRank: 'hc_ironman_ranks.rank',
+      ultimateIronmanRank: 'ult_ironman_ranks.rank',
       groupIronmanRank: 'gim_ranks.rank',
+      hardcoreGroupIronmanRank: 'hc_gim_ranks.rank',
       normalRank: 'normal_ranks.rank',
     })
     .from('all_ranks')
     .leftJoin('ironman_ranks', 'ironman_ranks.username', 'all_ranks.username')
+    .leftJoin('hc_ironman_ranks', 'hc_ironman_ranks.username', 'all_ranks.username')
+    .leftJoin('ult_ironman_ranks', 'ult_ironman_ranks.username', 'all_ranks.username')
     .leftJoin('gim_ranks', 'gim_ranks.username', 'all_ranks.username')
+    .leftJoin('hc_gim_ranks', 'hc_gim_ranks.username', 'all_ranks.username')
     .leftJoin('normal_ranks', 'normal_ranks.username', 'all_ranks.username')
     .whereRaw('LOWER(all_ranks.username) = ?', [paramsUsername.toLowerCase()])
     .first();
 
-  return response(200, { ranks });
+  return response(200, { ...ranks });
 };
 
 export const handler = middleware(getAccountTypeRanksByUsername);
