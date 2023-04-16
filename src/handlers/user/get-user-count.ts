e839dd1c3e8414f8479078c8fd7b1f9a2da056c5
Evan Sloan <evan@evans.loan>
2023-04-15 22:46:09 -0400

    omit deleted users in user count

--- src/handlers/user/get-user-count.ts
+++ src/handlers/user/get-user-count.ts
@@ -5,7 +5,9 @@ import { CollectionLogUser } from '@models/index';
 import { response } from '@utils/handler-utils';
 
 const getUserCount: APIGatewayProxyHandlerV2 = async () => {
-  const count = await CollectionLogUser.query().resultSize();
+  const count = await CollectionLogUser.query()
+    .where({ deleted_at: null })
+    .resultSize();
 
   return response(200, { count });
 };
