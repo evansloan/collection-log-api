954ef5a1647aadc8f3f599d1c45aa2136ebab724
Evan Sloan <evan@evans.loan>
2023-05-06 15:04:42 -0400

    implement item modifiers update function formatting rename getItems -> getItemsWithRelated add getItems rename getKillCounts -> getKillCountsWIthRelated add getKillCounts

--- src/dao/CollectionLogDao.ts
+++ src/dao/CollectionLogDao.ts
@@ -5,44 +5,84 @@ import { CustomQueryBuilder } from '@lib/custom-query-builder';
 
 export default class CollectionLogDao {
   private collectionLog?: CollectionLog;
+  private genderModifier = 'maleItems';
   private query: CustomQueryBuilder<CollectionLog> = CollectionLog.query();
 
   constructor(collectionLog?: CollectionLog) {
     if (collectionLog) {
-      this.collectionLog = collectionLog;
+      this.init(collectionLog);
     }
   }
 
-  async getByUsername(username: string) {
-    this.collectionLog = await this.query
+  private init = (collectionLog?: CollectionLog) => {
+    this.collectionLog = collectionLog;
+    const isFemale = collectionLog?.user.isFemale;
+    this.genderModifier = isFemale ? 'femaleItems' : 'maleItems';
+  };
+
+  getByUsername = async (username: string) => {
+    const collectionLog = await this.query
       .withGraphJoined('user')
       .findOne(raw('LOWER(??)', 'user.username'), '=', username.toLowerCase())
       .orderBy('updated_at', 'DESC');
+    this.init(collectionLog);
 
     return this.collectionLog;
-  }
+  };
 
-  async getByAccountHash(accountHash: string) {
-    this.collectionLog = await this.query
+  getByAccountHash = async (accountHash: string) => {
+    const collectionLog = await this.query
       .withGraphJoined('user')
       .findOne({ account_hash: accountHash })
       .orderBy('updated_at', 'DESC');
+    this.init(collectionLog);
 
     return this.collectionLog;
-  }
+  };
 
-  async getItems() {
-    const items = await this.collectionLog?.$relatedQuery('items')
+  getItemsWithRelated = async (filterGender: boolean) => {
+    const query = this.collectionLog?.$relatedQuery('items')
+      .modify(this.genderModifier)
       .withGraphJoined('page.[tab]')
       .orderBy('page:tab.name', 'ASC')
       .orderBy('page.name', 'ASC')
       .orderBy('sequence', 'ASC');
 
-    return items;
-  }
+    if (filterGender) {
+      query?.modify(this.genderModifier);
+    }
 
-  async getObtainedItems() {
-    const items = await this.collectionLog?.$relatedQuery('items')
+    return await query;
+  };
+
+  getItems = async (select?: any, pageName?: string, limit?: number, offset?: number) => {
+    const query = this.collectionLog?.$relatedQuery('items')
+      .modify(this.genderModifier)
+      .joinRelated('page')
+      .orderBy('sequence', 'ASC');
+
+    if (select) {
+      query?.select(select);
+    }
+
+    if (pageName) {
+      query?.where(raw('LOWER(??)', 'page.name'), pageName.toLowerCase());
+    }
+
+    if (limit) {
+      query?.limit(limit);
+    }
+
+    if (offset) {
+      query?.offset(offset);
+    }
+
+    return await query;
+  };
+
+  getObtainedItems = async (limit?: number) => {
+    const query = this.collectionLog?.$relatedQuery('items')
+      .modify(this.genderModifier)
       .select({
         id: 'item_id',
         obtained: raw('BOOL_OR(obtained)'),
@@ -52,13 +92,16 @@ export default class CollectionLogDao {
       .max('obtained_at AS obtained_at')
       .where('obtained', true)
       .groupBy('item_id')
-      .orderBy(raw('MAX(obtained_at)'), 'DESC')
-      .limit(5);
+      .orderBy(raw('MAX(obtained_at)'), 'DESC');
 
-    return items;
-  }
+    if (limit) {
+      query?.limit(limit);
+    }
 
-  async getKillCounts() {
+    return await query;
+  };
+
+  getKillCountsWithRelated = async () => {
     const killCounts = await this.collectionLog?.$relatedQuery('killCounts')
       .withGraphJoined('page.[tab]')
       .orderBy('page:tab.name', 'ASC')
@@ -66,9 +109,33 @@ export default class CollectionLogDao {
       .orderBy('sequence', 'ASC');
 
     return killCounts;
-  }
+  };
 
-  async getFormattedCollectionLog(user?: CollectionLogUser) {
+  getKillCounts = async (select?: any, pageName?: string, limit?: number, offset?: number) => {
+    const query = this.collectionLog?.$relatedQuery('killCount')
+      .joinRelated('page')
+      .orderBy('sequence', 'ASC');
+
+    if (select) {
+      query?.select(select);
+    }
+
+    if (pageName) {
+      query?.where(raw('LOWER(??)', 'page.name'), pageName.toLowerCase());
+    }
+
+    if (limit) {
+      query?.limit(limit);
+    }
+
+    if (offset) {
+      query?.offset(offset);
+    }
+
+    return await query;
+  };
+
+  getFormattedCollectionLog = async (user?: CollectionLogUser) => {
     if (!this.collectionLog) {
       return undefined;
     }
@@ -79,8 +146,8 @@ export default class CollectionLogDao {
     }
 
     const { username, accountType } = user;
-    const items = await this.getItems();
-    const killCounts = await this.getKillCounts();
+    const items = await this.getItemsWithRelated(true);
+    const killCounts = await this.getKillCountsWithRelated();
 
     const res: any = {
       collectionLogId: id,
@@ -156,5 +223,5 @@ export default class CollectionLogDao {
     });
 
     return res;
-  }
+  };
 }
